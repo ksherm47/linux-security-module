dc487c4da430aa9c89ffac94f42270da758f182a
Alex Myers <alex.myers@wustl.edu>
2022-04-01 19:40:45 -0500

    comment cleanup

--- kenlex.h
+++ kenlex.h
@@ -4,10 +4,12 @@
 // struct to handle what files we're watching
 // Definitely needs name of file, and might as well have name length of the file
 
+// what events we watch for. Could be edited to include frequencies, and low/high priority
 struct kenlex_event {
 	int id;
 };
 
+// defines different files being watched, modeled after inotify struct
 struct kenlex_watch_info {
 	struct kenlex_event fse;
 	//u32 mask;
@@ -17,6 +19,7 @@ struct kenlex_watch_info {
 	char name[];
 };
 
+// used to build linked list of all settings
 struct kenlex_watch_list_item {
     struct list_head list_node;
     struct kenlex_watch_info info;
